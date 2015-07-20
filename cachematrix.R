35abdab2e282849a8e47def331a241fe837b7037
jasmeetgujral <gujralsjasmeet@gmail.com>
2015-07-20 13:31:21 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+#makeCacheMatrix creats special matrix to cache inverse
+#it returns list of functions to store and get matrix and its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(xmtx = matrix()) {
+  #xmtx is the matrix for whcih inverse is to be calculates
+  
+  inv <- NULL
+  #inverse set to NUll at the initiation.
+  
+  set <- function(ymtx) {
+    #function to store new matrix
+    xmtx <<- ymtx
+    inv <<- NULL
+    #as new matrix is stored its inverse is again set to Null
+  }
+  
+  get <- function() xmtx
+  #get function to call stored matrix 
+  
+  setinv <- function(minverse) inv <<- minverse
+  #storing inverse for matrix stored
+  
+  getinv <- function() inv
+  #to call inverse already stored
+  
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
+  #list of fucntion set as output for makeCacheMatrix
 }
 
 
-## Write a short comment describing this function
+#cachesolve function computes inverse or pick value from cache if already computed
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, xmtx, ...) {
+  #two arguments: 1. Output list vector from makeCacheMatrix
+  #2. Matric for which inverse needs to be calculated
+  
+  inv <- x$getinv()
+  #using x$getinv() to get stored inverse
+  
+  if(!is.null(inv) & (all.equal(x$get,xmtx))) {
+    #if stored inverse is not null and 
+    #stored matrix is same as input matrix
+    #inverse is returned as stored value(from cache)
+        message("getting cached data")
+    return(inv)
+  }
+  
+  #if matrix inverse is not stored in cache
+  
+  x$set(xmtx)
+  #set stored matrix to new matrix
+  
+  inv <- solve(xmtx, ...)
+  #calculate inverse of new matrix
+  
+  x$setinv(inv)
+  #store new inverse as cache
+  
+  inv
+  #return inverse of a matrix
 }
